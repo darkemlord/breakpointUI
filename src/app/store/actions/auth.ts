fe77d9cc190a495f4a3dabd84eb14ab4722c9302
emanuel caro <darkemlord@gmail.com>
2022-12-11 01:01:41 +0900

    006 starting UI

--- src/app/store/actions/auth.ts
+++ src/app/store/actions/auth.ts
@@ -1,20 +1,23 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-export const signup = createAsyncThunk<any, any>("/signup", async (params) => {
-  try {
-    const response = await axios({
-      method: "post",
-      url: "http://localhost:3000/signup",
-      data: {
-        user: {
-          email: params.email,
-          password: params.password,
+export const signup = createAsyncThunk<any, any>(
+  "/register",
+  async (params) => {
+    try {
+      const response = await axios({
+        method: "post",
+        url: "http://localhost:3000/users",
+        data: {
+          user: {
+            email: params.email,
+            password: params.password,
+          },
         },
-      },
-    });
-    return response.data;
-  } catch (error) {
-    return error;
+      });
+      return response.data;
+    } catch (error) {
+      return error;
+    }
   }
-});
+);
